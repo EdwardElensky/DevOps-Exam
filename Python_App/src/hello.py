f9db6e48a89967984218fe01e4f839f8b16c9363
Ed Elensky <elenskyed516@gmail.com>
2021-04-11 20:21:53 +0300

    test

--- Python_App/src/hello.py
+++ Python_App/src/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World 1! 111:-)'
+    return 'Hello World 1! 111111:-)'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
